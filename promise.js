2a1b2b40b737497379d61aa45c754f63bccb082a
TAMJIDA AKTER <162369026+tamjidaWeb@users.noreply.github.com>
2025-09-02 19:56:33 +0600

    c

---
+++ promise.js
@@ -0,0 +1,7 @@
+// const loadData=()=>{
+//     fetch('https://jsonplaceholder.typicode.com/todos/1')
+//     .then(res=>res.json())
+//     .then(data=>console.log(data))
+//     .catch(err=>console.log(err))
+// }
+// loadData();

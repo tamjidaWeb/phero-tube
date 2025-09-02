bb74599f4a585c85568230035f2c8fa708a759e6
TAMJIDA AKTER <162369026+tamjidaWeb@users.noreply.github.com>
2025-09-02 23:02:16 +0600

    c

--- promise.js
+++ promise.js
@@ -64,11 +64,26 @@
 //     .catch(err=>console.log(err))
 
 
+const loadData=async()=>{
+    console.log('ami prothom');
+    console.log('ami second');
 
+    const response = await fetch('https://jsonplaceholder.typicode.com/todos/1')
+    const data = await response.json();
 
+    console.log(data)
+  
+    const result = 10+10;
+    console.log(result);
 
+    console.log('ami third')
+}
 
+loadData()
 
+//   fetch('https://jsonplaceholder.typicode.com/todos/1')
+//         .then(res=>res.json())
+        // .then(data=>console.log(data))
 
 
 

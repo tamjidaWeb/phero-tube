5f09896fd64e9e609d0d75021a3b84dd3231274c
TAMJIDA AKTER <162369026+tamjidaWeb@users.noreply.github.com>
2025-09-02 22:45:48 +0600

    c

--- promise.js
+++ promise.js
@@ -45,23 +45,23 @@
 
 
 
-const urls=[
-    'https://jsonplaceholder.typicode.com/posts',
-    'https://jsonplaceholder.typicode.com/comments',
-    'https://jsonplaceholder.typicode.com/albums',
-    'https://jsonplaceholder.typicode.com/photos',
-    'https://jsonplaceholder.typicode.com/todos',
-];
+// const urls=[
+//     'https://jsonplaceholder.typicode.com/posts',
+//     'https://jsonplaceholder.typicode.com/comments',
+//     'https://jsonplaceholder.typicode.com/albums',
+//     'https://jsonplaceholder.typicode.com/photos',
+//     'https://jsonplaceholder.typicode.com/todos',
+// ];
 
 
-Promise.all(urls.map(url=>{
-    return fetch(url)
-    .then(res=>res.json)
-    .then(data=>data)
-}))
-    .then(res=>res)
-    .then(data=>console.log(data))
-    .catch(err=>console.log(err))
+// Promise.all(urls.map(url=>{
+//     return fetch(url)
+//     .then(res=>res.json)
+//     .then(data=>data)
+// }))
+//     .then(res=>res)
+//     .then(data=>console.log(data))
+//     .catch(err=>console.log(err))
 
 
 

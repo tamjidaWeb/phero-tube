289ab3ae4cb5e46e35b66e3064b66fdaf3d6093e
TAMJIDA AKTER <162369026+tamjidaWeb@users.noreply.github.com>
2025-09-02 22:44:05 +0600

    c

--- promise.js
+++ promise.js
@@ -1,7 +1,80 @@
-// const loadData=()=>{
-//     fetch('https://jsonplaceholder.typicode.com/todos/1')
+// // const loadData=()=>{
+// //     fetch('https://jsonplaceholder.typicode.com/todos/1')
+// //     .then(res=>res.json())
+// //     .then(data=>console.log(data))
+// //     .catch(err=>console.log(err))
+// // }
+// // loadData();
+
+// const fetchData = () =>{
+//     return new Promise((resolve,reject)=>{
+//         const status = true;
+//         if(status){
+//             const mockData = {
+//                 json:()=>Promise.resolve({name:'hero'})
+//             }
+//             resolve(mockData)
+//         }
+//         else{
+//             reject('server error')
+//         }
+//     })
+// }
+
+// fetchData()
 //     .then(res=>res.json())
 //     .then(data=>console.log(data))
 //     .catch(err=>console.log(err))
-// }
-// loadData();
+
+
+//     const fetchData2 = () =>{
+//         return new Promise((resolve,reject)=>{
+//             const status = false;
+//             if(status){
+//                 resolve({name:'hero'})
+//             }
+//             else{
+//                 reject('data is not found')
+//             }
+//         })
+//     }
+
+//     fetchData2()
+//         .then(res=>console.log(res))
+//         .catch(err=>console.log(err))
+
+
+
+const urls=[
+    'https://jsonplaceholder.typicode.com/posts',
+    'https://jsonplaceholder.typicode.com/comments',
+    'https://jsonplaceholder.typicode.com/albums',
+    'https://jsonplaceholder.typicode.com/photos',
+    'https://jsonplaceholder.typicode.com/todos',
+];
+
+
+Promise.all(urls.map(url=>{
+    return fetch(url)
+    .then(res=>res.json)
+    .then(data=>data)
+}))
+    .then(res=>res)
+    .then(data=>console.log(data))
+    .catch(err=>console.log(err))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
